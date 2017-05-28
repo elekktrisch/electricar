ec787063223441f712cf28ea515f033ee077f40c
Roman Schlegel <roman.schlegel@gmail.com>
2017-05-28 18:37:02 +0200

    changed plug images to webpack require for bundling

--- app/scripts/knowhow/knowhow-controller.js
+++ app/scripts/knowhow/knowhow-controller.js
@@ -3,6 +3,24 @@ export class KnowhowCtrl {
         $scope.term = $routeParams.term;
         $anchorScroll.yOffset = 0;   // always scroll by extra pixels
         $location.hash($scope.term);
+        let images = [
+            "wall_t13",
+            "wall_schuko",
+            "caravan",
+            "wall_cee",
+            "cee64",
+            "auto_type1",
+            "auto_type2",
+            "auto_type3",
+            "auto_chademo",
+            "auto_ccs",
+            "tesla-supercharger"
+        ];
+
+        $scope.image = [];
+        for (let i = 0; i < images.length; i++) {
+            $scope.image[images[i]] = require(`../../images/${images[i]}.jpg`);
+        }
     }
 }
 
