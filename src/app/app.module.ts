7abeed11865f21605b20f08e0656b7c33f5cbbd3
Roman Schlegel <romanschlegel@gmail.com>
2017-06-11 14:53:37 +0200

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CarsComponent } from './cars/cars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule
